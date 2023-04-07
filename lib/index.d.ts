12216d64bf057e2e8bd6cdf97189e3fac8c24b04
Robert Corponoi <robertcorponoi@gmail.com>
2023-04-06 20:22:58 -0400

    Removed lib directory since dist is now where the bundled files are placed

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PixiMatter } from "./pixi-matter";
-import { PhysicsSprite } from "./physics_objects/physics_sprite";
-import { PhysicsGraphics } from "./physics_objects/physics_graphics";
-export { PixiMatter, PhysicsSprite, PhysicsGraphics };
